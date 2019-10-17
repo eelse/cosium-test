0f3c037029d2dafe69af506d988c958f9b7661b0
AMS\Pieters_E <eelse.pieters@schiphol.nl>
2019-10-17 10:05:20 +0200

    Setup

---
+++ src/main/java/eu/eelse/test/TestTheThing.java
@@ -0,0 +1,12 @@
+package eu.eelse.test;
+
+public class TestTheThing {
+
+  /**
+   * This is some arbitrary long javadoc that gets truncated:
+   * ####################################################
+   */
+  public static void main(String[] arg) {
+    // TODO
+  }
+}
